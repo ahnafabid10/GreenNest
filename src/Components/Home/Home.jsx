f6d92c0dae3d148906941f9aa9e82acf4fbd4af3
Ahnaf Abid <ahnafabid600@gmail.com>
2025-11-06 15:24:26 +0600

    fix footer

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar';
+import Footer from '../Footer/Footer';
+import { Outlet } from 'react-router';
 
 const Home = () => {
     return (
         <div>
             <NavBar></NavBar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
