58a95c97b420e421f493ad7bd117110423c34df2
Ahnaf Abid <ahnafabid600@gmail.com>
2025-11-07 21:01:57 +0600

    create context and authprovider

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { Children } from 'react';
+import { AuthContext } from './AuthContext';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../Firebase/Firebase.config';
 
-const AuthProvider = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const AuthProvider = ({children}) => {
+
+    // Create user with email and password
+    const createUser =(email, password) =>{
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    
+
+    const authData = {
+        createUser,
+
+    }
+    return <AuthContext value={authData}>
+        {children}
+    </AuthContext>
 };
 
 export default AuthProvider;
